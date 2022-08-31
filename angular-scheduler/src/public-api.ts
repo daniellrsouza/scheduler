672099c0959730f6294b60796bea850af91f3de8
Daniel Souza <daniel.rodrigues@infosys.com>
2022-08-31 01:33:53 -0300

    chore: add pipes to top level import

--- angular-scheduler/src/public-api.ts
+++ angular-scheduler/src/public-api.ts
@@ -4,3 +4,5 @@
 
 export * from './lib/scheduler/scheduler.component';
 export * from './lib/scheduler/scheduler.module';
+export * from './lib/scheduler/current-row.pipe';
+export * from './lib/scheduler/generate-array.pipe';
